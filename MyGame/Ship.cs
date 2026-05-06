09e79a64596acee3816fe940a830b43e3f396e2f
WillMallory28 <wpmallory@outlook.com>
2026-05-06 08:33:49 -0700

    modified:   MyGame/GameScene.cs 	new file:   MyGame/Laser.cs 	new file:   MyGame/Meteor.cs 	new file:   MyGame/MeteorSpawner.cs 	modified:   MyGame/MyGame.csproj 	new file:   MyGame/Ship.cs

---
+++ MyGame/Ship.cs
@@ -0,0 +1,57 @@
+using System.Data.Common;
+using GameEngine;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+namespace MyGame
+{
+    public class Ship : GameObject
+    {
+        private const float Speed = 0.3f;
+        private const int FireDelay = 200;
+        private int _fireTimer = 0;
+        private readonly Sprite _sprite = new Sprite();
+        // Creates our ship
+        public Ship()
+        {
+            _sprite.Texture = Game.GetTexture("Resources/ship.png");
+            _sprite.Position = new Vector2f(100, 100);
+        }
+        // Draws our ship
+        public override void Draw()
+        {
+            Game.RenderWindow.Draw(_sprite);
+        }
+        // Updates our ship every frame
+        public override void Update(Time elapsed)
+        {
+            Vector2f pos = _sprite.Position;
+            float x = pos.X;
+            float y = pos.Y;
+            int msElapsed = elapsed.AsMilliseconds();
+
+            if (Keyboard.IsKeyPressed(Keyboard.Key.W)) { y -= Speed * msElapsed; }
+            if (Keyboard.IsKeyPressed(Keyboard.Key.S)) { y += Speed * msElapsed; }
+            if (Keyboard.IsKeyPressed(Keyboard.Key.A)) { x -= Speed * msElapsed; }
+            if (Keyboard.IsKeyPressed(Keyboard.Key.D)) { x += Speed * msElapsed; }
+
+            _sprite.Position = new Vector2f(x, y);
+
+            if (_fireTimer > 0)
+            {
+                _fireTimer -= msElapsed;
+            }
+            if(Keyboard.IsKeyPressed(Keyboard.Key.Space) && _fireTimer <=0)
+            {
+                _fireTimer = FireDelay;
+            
+            FloatRect bounds = _sprite.GetGlobalBounds();
+            float laserX = x + bounds.Width;
+            float laserY = y + bounds.Height / 2.0f;
+            Laser laser = new Laser(new Vector2f(laserX, laserY));
+            Game.CurrentScene.AddGameObject(laser);
+            }
+        }
+    }
+}
+
